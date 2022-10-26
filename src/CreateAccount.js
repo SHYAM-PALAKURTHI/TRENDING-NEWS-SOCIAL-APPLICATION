96066c57669f8477d6f742fe333ed6e5fa7cda46
Vikashnaidu007 <115589583+Vikashnaidu007@users.noreply.github.com>
2022-10-26 19:34:48 -0400

    Create CreateAccount.js

---
+++ src/CreateAccount.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox
+  }
+  from 'mdb-react-ui-kit';
+import "./CreateAccount.css";
+import { Link ,useNavigate} from "react-router-dom";
+import axios from "axios";
