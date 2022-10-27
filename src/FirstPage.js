556850250dadac3d6a059d90f24b326047b7f415
PranaviChacham <115848719+PranaviChacham@users.noreply.github.com>
2022-10-26 21:02:09 -0400

    Create FirstPage.js

---
+++ src/FirstPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet} from "react-router-dom";
+import "./FirstPage.css";
+
+function FirstPage()
+{
+    return(
+    <div className="FirstPageComponent">
+        <div>
+            <div className="WelcomeHeading">Welcome to Trending News Social Appliction</div>
+        </div>
+        
+        <Outlet/>
+    </div>
+    )
+
+}
+
+export default FirstPage;
