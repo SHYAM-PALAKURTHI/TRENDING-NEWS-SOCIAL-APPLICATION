7c72dec5315fadd62e8ad965f5d5d138a5f1aa21
PranaviChacham <115848719+PranaviChacham@users.noreply.github.com>
2022-11-14 17:23:55 -0500

    Update FirstPage.js

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -7,7 +7,7 @@ function FirstPage()
     return(
     <div className="FirstPageComponent">
         <div>
-            <div className="WelcomeHeading">Welcome to Trending News Social Appliction</div>
+            <div className="WelcomeHeading">Welcome to Trending News Social Application</div>
         </div>
         
         <Outlet/>
