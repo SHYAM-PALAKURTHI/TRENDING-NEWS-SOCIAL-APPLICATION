292a8782b442afd96bcf54a465e5d91a35e675bd
saitheja285 <117962833+saitheja285@users.noreply.github.com>
2022-12-06 11:56:51 -0500

    sprint 3 update

--- Server/index.js
+++ Server/index.js
@@ -1,284 +1,72 @@
-const express = require('express');
-const mysql = require("mysql");
-const app = express();
-const port = 8000;
-const fs = require("fs");
-var cors = require('cors');
-var Jwt = require('jsonwebtoken');
-const { verify } = require('crypto');
-var jwtKey = "Key-NewsApp";
-
-
-app.use(cors());
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "project20222402",
-    insecureAuth: true
-});
-
-connection.connect();
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send("server working")
-})
-
-app.get("/check_login", (req, res) => {
-
-    const data = req.query;
-    connection.query(`select UserId from userdetails where MailId = "${data.id}" && Passsword = "${data.password}"; `, (err, result) => {
-        if (err) {
-            res.status(401).send();
-        }
-
-        else {
-            if (result.length != 0) {
-                var UserId = result[0].UserId;
-                Jwt.sign({ UserId }, jwtKey, (e, token) => {
-                    if (e) {
-                        res.status(401).send();
-                    }
-
-                    else {
-                        res.send({ UserId: UserId, token: token });
-                    }
-                })
-            }
-            else {
-                res.status(401).send();
-            }
-        }
-
-    })
-})
-
-
-app.get("/check_google_login", (req, res) => {
-
-    const data = req.query;
-    connection.query(`select MailId from userdetails where UserId="GI-${data.googleId}"; `, (err, result) => {
-        if (err) {
-            res.status(401).send();
-        }
-
-        else {
-            var UserId = `GI-${data.googleId}`;
-            if (result.length == 0) {
-
-                connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId,profile_url) VALUES ('${data.name}', '${data.email}', 'Google Login', "GI-${data.googleId}","${data.imageUrl}");`, (errr, resu) => {
-
-                    if (errr) {
-                        res.status(401).send();
-
-                    }
-
-                    else {
-                        Jwt.sign({ UserId }, jwtKey, (e, token) => {
-                            if (e) {
-                                res.status(401).send();
-                            }
-
-                            else {
-                                res.send({ status: 200, UserId: UserId, token: token });
-                            }
-                        })
-                    }
-                })
-
-            }
-            else {
-                Jwt.sign({ UserId }, jwtKey, (e, token) => {
-                    if (e) {
-                        res.status(401).send();
-                    }
-
-                    else {
-                        res.send({ UserId: UserId, token: token });
-                    }
-                })
-            }
-        }
-
-    })
-})
-
-
-app.get("/sign_up", (req, res) => {
-
-    const data = req.query;
-
-
-    connection.query(`select UserId from userdetails where MailId = "${data.MailId}";`, (err, result) => {
-        if (err) {
-            res.status(401).send();
-        }
-        else {
-            if (result.length == 0) {
-                connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId) VALUES ('${data.FirstName}', '${data.MailId}', '${data.Password}', "NA-${data.MailId}");`, (errr, results) => {
-                    if (errr) {
-                        res.status(401).send();
-
-                    }
-
-                    else {
-                        var UserId = `NA-${data.MailId}`;
-                        Jwt.sign({ UserId }, jwtKey, (e, token) => {
-                            if (e) {
-                                res.status(401).send();
-                            }
-
-                            else {
-                                res.send({ UserId: UserId, token: token });
-                            }
-                        })
-                    }
-
-                })
-
-            }
-
-            else {
-                res.send({ UserId: "Email Id Used", token: "" });
-            }
-        }
-    })
-
-})
-
-app.get("/user_name", VerifyToken, (req, res) => {
-    const data = req.query;
-    const UserId = data.UserId;
-    connection.query(`select * from userdetails where UserId = "${UserId}"; `, (err, result) => {
-        if (err) {
-            res.status(401).send();
-        }
-        else {
-            if (result.length == 0) {
-                res.status(401).send();
-
-            }
-
-            else {
-                res.send(result[0]);
-
-            }
-        }
-    })
-
-
-
-})
-
-
-app.get("/edit_user_data", VerifyToken, (req, res) => {
-
-    const data = req.query;
-
-    connection.query(`select UserId from userdetails where MailId = "${data.mail}" and UserId != "${data.UserId}";`, (err, result) => {
-        if (err) {
-            res.status(401).send();
-        }
-        else {
-            if (result.length == 0) {
-                if (data.profile_url == undefined) {
-                    connection.query(`UPDATE userdetails SET FirstName="${data.name}",MailId="${data.mail}" WHERE UserId="${data.UserId}";`, (errr, results) => {
-                        if (errr) {
-
-                            res.status(401).send();
-
-                        }
-
-                        else {
-                            res.send({ message: "successfully" })
-                        }
-
-                    })
-
-                }
-                else {
-                    connection.query(`UPDATE userdetails SET FirstName="${data.name}",MailId="${data.mail}",profile_url="${data.profile_url}" WHERE UserId="${data.UserId}";`, (errr, results) => {
-                        if (errr) {
-
-                            res.status(401).send();
-
-                        }
-
-                        else {
-                            res.send({ message: "successfully" })
-                        }
-
-                    })
-
-                }
-
-
-            }
-
-            else {
-                res.send({ message: "Email Id Used" });
-            }
-        }
-    })
-})
-
-app.get("/edit_password", (req, res) => {
-    const data = req.query;
-    if (data.UserId.split("-")[0] == "GI") {
-        connection.query(`UPDATE userdetails SET Passsword="${data.NewPassword}" WHERE UserId="${data.UserId}";`, (errr, results) => {
-            if(errr)
-            {
-                res.status(401).send();
-            }
-            else
-            {
-                res.send({ message: "successfully" })
-            }
-        });
-
-    }
-    else {
-
-        connection.query(`UPDATE userdetails SET Passsword="${data.NewPassword}" WHERE UserId="${data.UserId}" and Passsword="${data.OldPassword}";`, (errr, results) => {
-            if(errr)
-            {
-                res.status(401).send();
-            }
-            else
-            {
-                res.send({ changedRows: results.changedRows })
-            }
-        });
-
-    }
-})
-
-
-function VerifyToken(req, res, next) {
-    let token = req.headers['authorization'];
-    if (token) {
-        Jwt.verify(token, jwtKey, (err, vaild) => {
-            if (err) {
-                res.status(401).send({ result: "Please Add Correct Token With Header" });
-
-            }
-            else {
-                next();
-            }
-
-        })
-    }
-
-    else {
-        res.status(403).send({ result: "Please Add Token With Header" });
-    }
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FirstPage from './FirstPage';
+import LoginPage from './LoginPage';
+import CreateAccount from './CreateAccount';
+import News from './news';
+import {
+  Setting,
+  ProfileSetting,
+  EditProfileSetting,
+  EditPassword,
+} from './Setting';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from 'react-router-dom';
+import Invite from './Invite';
+import Ratings from './Ratings';
+import WatchLater from './WatchLater';
+import SelectCategories from './SelectCategories';
+import UserProfile from './UserProfile';
+import SearchUserCards from './SearchUserCards';
+import PendingRequests from './PendingRequests';
+import UserFriendsOrFollowers from './UserFriendsOrFollowers';
+import LiveMatches from './LiveMatches';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstPage />}>
+          <Route index element={<Navigate to="login" />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="sign_up" element={<CreateAccount />} />
+          <Route
+            path="account_Create_succuss"
+            element={<Navigate to="/login" />}
+          />
+        </Route>
+
+        <Route path="/news/:id" element={<News />} />
+        <Route path="/setting" element={<Setting />}>
+          <Route index element={<Navigate to="profile" />} />
+          <Route path="profile" element={<ProfileSetting />} />
+          <Route path="edit_profile" element={<EditProfileSetting />} />
+          <Route path="edit_password" element={<EditPassword />} />
+        </Route>
+        <Route path="invite" element={<Invite />} />
+        <Route path="pending-requests" element={<PendingRequests />} />
+        <Route path="live-matches" element={<LiveMatches />} />
+        <Route path="search" element={<SearchUserCards />} />
+        <Route path="user/:id" element={<UserProfile />} />
+        <Route path="friends/:id" element={<UserFriendsOrFollowers />} />
+        <Route path="followers/:id" element={<UserFriendsOrFollowers />} />
+        <Route path="ratings/:id" element={<Ratings />} />
+        <Route path="watch-later/:id" element={<WatchLater />} />
+        <Route path="select-categories" element={<SelectCategories />} />
+      </Routes>
+    </Router>
+  );
 }
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
